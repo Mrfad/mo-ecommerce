265043c161222822a61eb6f9711c42b95b5b95e1
mrfad <fady.ghalayiny@outlook.com>
2021-11-07 17:42:41 +0200

    finished 4

--- storefront/settings.py
+++ storefront/settings.py
@@ -84,10 +84,22 @@ WSGI_APPLICATION = 'storefront.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+# desktop home settings
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'moecommercedb', 
+        'USER': 'postgres', 
+        'PASSWORD': 'Microwave',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
